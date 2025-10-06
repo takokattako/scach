b607bfcd0fb0336a7ef2aa59c0b1660a7b7f7eae
takokattako <sshuku221081@shukutoku.ed.jp>
2025-10-06 18:20:23 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,7 @@
-let ballX;
-let ballY;
-let ballSpeedX;
-let ballSpeedY;
-let ballradius;
-let paddleX;
-let paddleY;
-let paddleSpeed; 
-let paddleWidth; 
-let paddleHeight;
+let ballX, ballY, ballSpeedX, ballSpeedY;
+let ballAX, ballAY, ballSpeedAX, ballSpeedAY;
+let ballRadius;
+let paddleX, paddleY, paddleSpeed, paddleWidth, paddleHeight;
 
 function setup() {
     createCanvas(600, 400);
@@ -15,6 +9,10 @@ function setup() {
     ballY = height / 2;
     ballSpeedX = 5;
     ballSpeedY = -5;
+    ballAX = width / 2 - 100;
+    ballAY = height / 2 - 100;
+    ballSpeedAX = 3;
+    ballSpeedAY = 3;
     ballRadius = 15;
     paddleX = width / 2;
     paddleY = height - 30;
@@ -27,6 +25,8 @@ function draw() {
     background(0);
     ballX = ballX + ballSpeedX;
     ballY = ballY + ballSpeedY;
+    ballAX = ballAX + ballSpeedAX;
+    ballAY = ballAY + ballSpeedAY;
     
     // パドルを動かす
     if (keyIsDown(LEFT_ARROW)) {
@@ -36,35 +36,48 @@ function draw() {
         paddleX = paddleX + paddleSpeed
     }
 
-    // パドルに当たったら
-    if (paddleX < ballX && 
-        ballX < paddleX + paddleWidth && 
-        paddleY - ballRadius < ballY && 
-        ballY < paddleY) {
-            ballSpeedY = -ballSpeedY;
+    // パドルに当たったら（2つ分）
+    if (paddleX < ballX && ballX < paddleX + paddleWidth && paddleY - ballRadius < ballY && ballY < paddleY) {
+        ballSpeedY = -ballSpeedY;
+    }
+    if (paddleX < ballAX && ballAX < paddleX + paddleWidth && paddleY - ballRadius < ballAY && ballAY < paddleY) {
+        ballSpeedAY = -ballSpeedAY;
     }
 
     // 右に当たったら
     if (width < ballX + ballRadius) {
         ballSpeedX = -ballSpeedX;
     }
+    if (width < ballAX + ballRadius) {
+        ballSpeedAX = -ballSpeedAX;
+    }
     // 左に当たったら
     if (ballX - ballRadius < 0) {
-    ballSpeedX = -ballSpeedX;
+        ballSpeedX = -ballSpeedX;
     }
-     // 上に当たったら
+    if (ballAX - ballRadius < 0) {
+        ballSpeedAX = -ballSpeedAX;
+    }
+    // 上に当たったら
     if (ballY - ballRadius < 0) {
         ballSpeedY = -ballSpeedY;
     }
+    if (ballAY - ballRadius < 0) {
+        ballSpeedAY = -ballSpeedAY;
+    }
 
-    // 下に当たったら
-    if (ballY + ballRadius > height) {
+    // 下に当たったら（どちらか1つでも）
+    if (ballY + ballRadius > height || ballAY + ballRadius > height) {
         ballSpeedX = 0;
         ballSpeedY = 0;
-        Fiil("white");
+        ballSpeedAX = 0;
+        ballSpeedAY = 0;
+        fill("white");
         textAlign(CENTER);
         text("GAME OVER", width / 2, height / 2);
     }
-        circle(ballX, ballY, ballRadius * 2);
-        rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    // 2つのボール描画
+    circle(ballX, ballY, ballRadius * 2);
+    circle(ballAX, ballAY, ballRadius * 2);
+    rect(paddleX, paddleY, paddleWidth, paddleHeight);
 }
